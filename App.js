d9d48f5c6924e4c5364766db1fa878ee38c76aa9
KaiTechRNStudent <tunmiseedu@yahoo.com>
2017-12-07 12:09:12 +0100

    12:09 update

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
+
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
